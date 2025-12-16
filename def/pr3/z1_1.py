01792f08c72e508b36bb56097b7ff24b81488aaf
ImshPolina <126556131+ImshPolina@users.noreply.github.com>
2025-12-16 23:31:46 +0300

    Update z1_1.py

--- def/pr3/z1_1.py
+++ def/pr3/z1_1.py
@@ -1,4 +1,13 @@
-a, b, c = int(input("Введите первое число: ")), int(input("Введите второе число: ")), int(input("Введите третье число: "))
+a = int(input("Введите первое число: "))
+b = int(input("Введите второе число: "))
+c = int(input("Введите третье число: "))
 
 print("Числа, принадлежащие интервалу [1, 3]:")
-[print(num) for num in [a, b, c] if 1 <= num <= 3]
+found = False
+for num in [a, b, c]:
+    if 1 <= num <= 3:
+        print(num)
+        found = True
+
+if not found:
+    print("Таких чисел нет")
